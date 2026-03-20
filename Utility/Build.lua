0ec4a74558fb03743766a30593c6bb981926a747
Awaken <93601997+Awakenchan@users.noreply.github.com>
2026-03-21 00:13:37 +0500

    Update Build.lua

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -1,9 +1,10 @@
 return {
-    __version = "3.5",
+    __version = "3.6",
     __log = {
         "[+] fixed filtering",
         "[+] added new decompiler if it doesnt exist",
         "[+] better stability",
         "[/] readded extra scripts",
+        "[+] added function tagging",
     }
 }
